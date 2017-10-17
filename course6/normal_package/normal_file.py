8016d52c6c4b8c5fe0d8b90d73d600716d8fc599
golem131 <golem131@gmail.com>
2017-10-17 16:17:38 +0300

    Lecture 6

---
+++ course6/normal_package/normal_file.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class MyClass:
+    pass
+
+
+def my_function():
+    print('My function is working!')
+
+
+GLOBAL_VAR = 12
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    my_function()
