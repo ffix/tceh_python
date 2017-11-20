5a3acd457f153517c9a273dd630386df46dfe90b
golem131 <golem131@gmail.com>
2017-11-20 19:58:59 +0300

    Lecture16

---
+++ course16/functions.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function log(value) {
+    console.log(value);
+    return 'as';
+}
+
+
+var namedLog = function(value) {
+    console.log('named', value);
+    return 4;
+};
+
+
+var nfe = function namedFunctionalExpression (i) {
+    if (i == 0) {
+        return 0;
+    }
+    // namedFunctionalExpression = null;
+    return i + namedFunctionalExpression(i - 1); // i-- or --i
+};
+
+var returnValue = log(123);
+console.log(returnValue);
+
+namedLog('asd');
+console.log(nfe(5));
+// namedFunctionalExpression(5);
+
+log = console.log;
+log();
+
+console.log(typeof namedLog);
+namedLog = null;
+// namedLog(23);
